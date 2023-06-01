32e2964f3d84b08a509410779a89ad68d2f6a318
Almir <nurm.almir@gmail.com>
2023-06-01 22:31:33 +0300

    ENH: wallpaper

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 someblocks & # /run/user/1000/dwl-bar-0 &
-swaybg -o "*" -i ~/Pictures/the-road-to-samarkand-by-thomas-thiemeyer-3840x2160-v0-vjtkt501abr81.png &
+swaybg -o "*" -i ~/Pictures/rock_2.png &
 dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus &
 mako &
