efaa3f1488c6da8270b6873a1edca2bde04fefb4
Almir <nurm.almir@gmail.com>
2023-06-25 22:14:03 +0300

    NEW: set_theme.sh script

---
+++ set_theme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# dark theme
+gsettings set org.gnome.desktop.interface cursor-theme GoogleDot-Blue
+gsettings set org.gnome.desktop.interface gtk-theme Material-Black-Blueberry
+gsettings set org.gnome.desktop.interface icon-theme breeze
+
+# light theme
+gsettings set org.gnome.desktop.interface cursor-theme Simp1e
+gsettings set org.gnome.desktop.interface gtk-theme Aurora-Nuevo-Orange
+gsettings set org.gnome.desktop.interface icon-theme Orange-Aurora
+
+
